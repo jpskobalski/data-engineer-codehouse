eb14892b97d367427e088e4a314cbd1f53fb6b17
juanskob <juan.skobalski@gmail.com>
2024-08-04 18:55:02 -0300

    removing pass

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ API_URL = "http://api.openweathermap.org/data/2.5/weather"
 API_KEY = "2954b118ada0935c62bf5fc06d622426"
 
 REDSHIFT_USER = "juan_skobalski_coderhouse"
-REDSHIFT_PASSWORD = "m03Z6nRz4b"
+REDSHIFT_PASSWORD = ""
 REDSHIFT_HOST = "data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com"
 REDSHIFT_PORT = "5439"
 REDSHIFT_DATABASE = "data-engineer-database"
